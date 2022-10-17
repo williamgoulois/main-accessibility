e1a5224b4f23ef3504949492b5e4d5cf4c5f95b4
William Goulois <william.goulois@fabernovel.com>
2022-10-17 15:29:57 +0200

    feat: add prefers-reduced-motion example

---
+++ src/components/SolidLogo/SolidLogo.tsx
@@ -0,0 +1,6 @@
+import solidJsLogo from "./logo.svg";
+import "./SolidLogo.css";
+
+export const SolidLogo = () => {
+  return <img src={solidJsLogo} class="h-16 w-16 rotate" alt="Solidjs logo" />;
+};
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { A } from "solid-start";
+import { SolidLogo } from "~/components/SolidLogo/SolidLogo";
 
 export default function Home() {
   return (
@@ -11,7 +12,7 @@ export default function Home() {
           Skip to footer
         </a>
         <nav>
-          <ul class="flex justify-center space-x-4 list-none">
+          <ul class="flex justify-center space-x-4 list-none flex-wrap">
             <li>
               <a href="#">Films</a>
             </li>
@@ -41,6 +42,7 @@ export default function Home() {
           <h1 class="text-4xl text-sky-700 font-thin uppercase mb-8">
             Hello from Solidjs!
           </h1>
+          <SolidLogo />
           <article>
             <p class="mb-4">
               Lorem ipsum dolor sit amet, officia excepteur ex fugiat
@@ -56,7 +58,9 @@ export default function Home() {
               velit enim. Voluptate laboris sint cupidatat ullamco ut ea
               consectetur et est culpa et culpa duis.
             </p>
-            <a class="skipLink p-2" href="#after-links">Skip links and go to next paragaph</a>
+            <a class="skipLink p-2" href="#after-links">
+              Skip links and go to next paragaph
+            </a>
             <ol class="mb-4 mx-4 list-decimal list-inside">
               <li>
                 <a href="#">Films</a>
@@ -141,7 +145,7 @@ export default function Home() {
       </main>
       <footer id="footer" class="mb-14">
         <nav>
-          <ul class="flex justify-center space-x-4 list-none">
+          <ul class="flex justify-center space-x-4 list-none flex-wrap">
             <li>
               <a href="#">Films</a>
             </li>
@@ -159,9 +163,6 @@ export default function Home() {
             </li>
             <li>
               <a href="#">Privatisation</a>
-            </li>
-            <li>
-              <a href="#">Espace CSE</a>
             </li>
             <li>
               <a href="#">Espace CSE</a>
